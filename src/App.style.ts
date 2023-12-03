29e17a3017068a6d81787b7b52bacbf6639257c9
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2023-12-03 15:04:56 -0300

    updates

--- src/App.style.ts
+++ src/App.style.ts
@@ -1,10 +1,24 @@
 import styled, { createGlobalStyle } from "styled-components";
+import GalaxyImage from "./assets/galaxy.png";
 
 export const AppContainer = styled.div`
   height: 100vh;
   width: 100vw;
 
   background-color: rgba(62, 73, 105, 255);
+
+  background-image: url(${GalaxyImage});
+  background-repeat: no-repeat;
+  background-size: 100%;
+
+  &::before {
+    content: "";
+    position: absolute;
+    height: 100vh;
+    width: 100vw;
+
+    backdrop-filter: saturate(0.5);
+  }
 `;
 
 export const GlobalStyle = createGlobalStyle`
